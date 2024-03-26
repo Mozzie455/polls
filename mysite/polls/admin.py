a17a5b5dd3e2ad94a2097a3b07b15de33eb5389d
Joshua Mutiso <joshuamutiso455@gmail.com>
2024-03-26 10:34:20 +0300

    fourth commit

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,7 +3,25 @@
 # polls/admin.py
 
 from django.contrib import admin
-from .models import Question
+from .models import Question, Choice
 # Register your models here.
 
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
+
 admin.site.register(Question)
+admin.site.register(Choice)
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
 # polls/views.py
+from django.db.models import F
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.views import generic
 from django.urls import reverse
 from django.utils import timezone
 
-from .models import Choices, Question
+from .models import Choice, Question
 
 # Create your views here.
 
@@ -41,7 +42,7 @@ def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
-    except (KeyError, Choices.DoesNotExist):
+    except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
         return render(
             request,
